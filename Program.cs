46917af662599aca4b2910474ecc361ec21c1eb3
Entra21 <entra21@proway.treina>
2020-07-07 21:05:19 -0300

    ignore

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace primeira_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myText = "meu texto";
+            var muNumber = 8;
+            var myPrice = 4.099;
+            var myBoolean = false;
+
+            Console.WriteLine("Voce e maior de idade");
+            var result = Console.ReadLine();
+
+            myBoolean = result == "sim";
+            Console.WriteLine(myBoolean);
+
+        }
+    }
+}
