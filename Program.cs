f14e28db71a3edd0a3901c8735934993f45c9247
Entra21 <entra21@proway.treina>
2020-07-09 21:37:27 -0300

    após markdown it

--- Program.cs
+++ Program.cs
@@ -8,16 +8,23 @@ namespace primeira_aula
         {
             Console.WriteLine("Qual é a sua idade?");
             var result = Console.ReadLine();
-            var userAge = Int32.Parse(result);
+
+            int userAge = Int32.Parse(result);
         
-            if (userAge > 17)
+            if (userAge > 15 && userAge < 51)
             {
-                Console.WriteLine("Sim! Eu sou maior de idade");
+                Console.WriteLine("Sim! Você tem idade para o Entra21");
             } 
+            // else if (userAge == 14 || userAge == 15)
+            // {
+            //     Console.WriteLine("Você não tem idade para o Entra21, mas tem idade para ser menor aprendiz");
+            // }
             else
             {
-                Console.WriteLine("Não! Eu sou menor de idade");
+                Console.WriteLine("Você não tem idade nem para o Entra21 e nem para ser menor aprendiz");
             }
+
+            Console.WriteLine("Só isso, mais nada!");
         }
     }
 }
